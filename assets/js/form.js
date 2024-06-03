c4b3726d4f46d1bd8ea381e87cf7d35b93236a51
Benja <benjamintomasvegaquiroz@gmial.com>
2024-06-03 18:53:22 -0400

    Arreglo de js

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,4 +1,4 @@
-const formOpenBtn = document.querySelector("#form-open"),
+const formOpenBtn = document.querySelector("#form_open"),
 home = document.querySelector(".home"),
 formContainer = document.querySelector(".form_container"),
 formCloseBtn = document.querySelector(".form_close"),
@@ -6,13 +6,22 @@ signupBtn = document.querySelector("#signup"),
 loginBtn = document.querySelector("#login"),
 pwShowHide = document.querySelectorAll(".pw_hide");
 
+
 formOpenBtn.addEventListener("click", () => home.classList.add("show"));
 formCloseBtn.addEventListener("click", () => home.classList.remove("show"));
 
-pwShowHide.forEach(icon => {
-   icon.addEventListener("click", () => {
-    
-   })
+
+pwShowHide.forEach((icon) => {
+  icon.addEventListener("click", () => { 
+    let getPwInput = icon.parentElement.querySelector("input");
+    if(getPwInput.type === "password"){
+        getPwInput.type = "text";
+        icon.classList.replace("uil-eye-slash", "uil-eye");
+    }else{
+        getPwInput.type = "password";
+        icon.classList.replace("uil-eye", "uil-eye-slash");
+    }
+    }) 
 });
 
 signupBtn.addEventListener("click", (e) => {
@@ -23,3 +32,5 @@ loginBtn.addEventListener("click", (e) => {
     e.preventDefault();
     formContainer.classList.remove("active");
 });
+
+

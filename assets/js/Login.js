f35a211a2f49a412aeb017b456c77097f230d867
Benja <benjamintomasvegaquiroz@gmial.com>
2024-06-03 01:25:55 -0400

    Arreglo codigo html js

--- assets/js/Login.js
+++ assets/js/Login.js
@@ -1,9 +1,25 @@
-const signInBtnLink = document.querySelector('.signInBtn-link');
+const formOpenBtn = document.querySelector("#form-open"),
+home = document.querySelector(".home"),
+formContainer = document.querySelector(".form_container"),
+formCloseBtn = document.querySelector(".form_close"),
+signupBtn = document.querySelector("#signup"),
+loginBtn = document.querySelector("#login"),
+pwShowHide = document.querySelectorAll(".pw_hide");
 
-const signUpBtnLink = document.querySelector('.signUnBtn-link');
+formOpenBtn.addEventListener("click", () => home.classList.add("show"));
+formCloseBtn.addEventListener("click", () => home.classList.remove("show"));
 
-const container = document.querySelector('.container');
+pwShowHide.forEach(icon => {
+   icon.addEventListener("click", () => {
+    
+   })
+});
 
-signUpBtnLink.addEventListener('click', () => {
-    container.classList.toggle('active');
+signupBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    formContainer.classList.add("active");
+});
+loginBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    formContainer.classList.remove("active");
 });
